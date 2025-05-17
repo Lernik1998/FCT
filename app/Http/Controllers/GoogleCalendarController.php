b6aa4e39e7b9e1e7d9d8cc831a6054e6232556e4
Lernik Gasparyan <lernik10@gmail.com>
2025-05-17 13:10:32 +0200

    1

--- app/Http/Controllers/GoogleCalendarController.php
+++ app/Http/Controllers/GoogleCalendarController.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Google\Client;
 use Google\Service\Calendar;
-use Illuminate\Support\Facades\Config;
+use Google\Service\Calendar\Event as GoogleEvent;
 use App\Models\Appointment;
 use Carbon\Carbon;
-use Log;
-// use Google\Service\Calendar\Event;
+use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
-
 use Spatie\GoogleCalendar\Event;
-
-
 
 class GoogleCalendarController extends Controller
 {
@@ -23,246 +18,158 @@ class GoogleCalendarController extends Controller
 
     public function __construct()
     {
-        $this->client = new Client();
-        $this->client->setApplicationName(config('google_calendar.application_name'));
-        $this->client->setScopes([
-            Calendar::CALENDAR,
-            Calendar::CALENDAR_EVENTS
-        ]);
-        // $this->client->setAccessType('offline');
-        // $this->client->setAuthConfig(config('google_calendar.credentials_path'));
-        // $this->service = new Calendar($this->client);
-
-        $this->client->setAccessType('offline');
-
-        // Usar las credenciales específicas para Calendar
-        // $this->client->setAuthConfig(config('google_calendar.credentials_path'));
-        $this->client->setAuthConfig(config('google-calendar.auth_profiles.service_account.credentials_json'));
-
-        // $this->client->setClientId(config('google_calendar.client_id')); // No es necesario para el service account FIXME:
-        $this->client->setClientSecret(config('google_calendar.client_secret'));
-        $this->client->setRedirectUri(config('google_calendar.redirect_uri'));
-
-        $this->service = new Calendar($this->client);
+        $this->initializeGoogleClient();
     }
 
-    // public function syncAppointment(Appointment $appointment)
-    // {
-    //     // try {
-    //     //     $event = new Calendar\Event([
-    //     //         'summary' => $appointment->title,
-    //     //         'description' => $appointment->description,
-    //     //         'start' => [
-    //     //             'dateTime' => $appointment->start->format('c'),
-    //     //             'timeZone' => config('app.timezone'),
-    //     //         ],
-    //     //         'end' => [
-    //     //             'dateTime' => $appointment->end->format('c'),
-    //     //             'timeZone' => config('app.timezone'),
-    //     //         ],
-    //     //     ]);
-
-    //     //     if ($appointment->google_calendar_event_id) {
-    //     //         // Update existing event
-    //     //         $event = $this->service->events->update(
-    //     //             config('google_calendar.calendar_id'),
-    //     //             $appointment->google_calendar_event_id,
-    //     //             $event
-    //     //         );
-    //     //     } else {
-    //     //         // Create new event
-    //     //         $event = $this->service->events->insert(
-    //     //             config('google_calendar.calendar_id'),
-    //     //             $event
-    //     //         );
-
-    //     //         $appointment->google_calendar_event_id = $event->id;
-    //     //         $appointment->save();
-    //     //     }
-
-    //     //     return $event;
-    //     // } catch (\Exception $e) {
-    //     //     \Log::error('Google Calendar sync error: ' . $e->getMessage());
-    //     //     throw $e;
-    //     // }
-
-    //     try {
-    //         $client = new \Google\Client();
-    //         $client->setApplicationName(config('google_calendar.application_name'));
-    //         $client->setScopes([
-    //             \Google\Service\Calendar::CALENDAR,
-    //             \Google\Service\Calendar::CALENDAR_EVENTS
-    //         ]);
-    //         $client->setAccessType('offline');
-    //         $client->setAuthConfig(config('google_calendar.credentials_path'));
-
-    //         $service = new \Google\Service\Calendar($client);
-
-    //         $event = new \Google\Service\Calendar\Event([
-    //             'summary' => $appointment->title,
-    //             'description' => $appointment->description,
-    //             'start' => [
-    //                 'dateTime' => $appointment->start->format('c'),
-    //                 'timeZone' => config('app.timezone'),
-    //             ],
-    //             'end' => [
-    //                 'dateTime' => $appointment->end->format('c'),
-    //                 'timeZone' => config('app.timezone'),
-    //             ],
-    //         ]);
-
-    //         if ($appointment->google_calendar_event_id) {
-    //             $event = $service->events->update(
-    //                 config('google_calendar.calendar_id'),
-    //                 $appointment->google_calendar_event_id,
-    //                 $event
-    //             );
-    //         } else {
-    //             $event = $service->events->insert(
-    //                 config('google_calendar.calendar_id'),
-    //                 $event
-    //             );
-
-    //             $appointment->google_calendar_event_id = $event->id;
-    //             $appointment->save();
-    //         }
-
-    //         return $event;
-    //     } catch (\Exception $e) {
-    //         \Log::error('Google Calendar sync error: ' . $e->getMessage());
-    //         throw $e;
-    //     }
-    // }
-
-    public function syncAppointment(Appointment $appointment)
+    protected function initializeGoogleClient()
     {
-        // Debugging
-        dd($appointment);
+        try {
+            $this->client = new Client();
+            $this->client->setApplicationName(config('google-calendar.application_name'));
+            $this->client->setScopes([
+                Calendar::CALENDAR,
+                Calendar::CALENDAR_EVENTS
+            ]);
 
-        return response()->json([
-            'success' => "Ha llegado a GoogleCalendarController.php"
-        ]);
+            // Configuración exclusiva para Service Account
+            $this->client->setAuthConfig(
+                config('google-calendar.auth_profiles.service_account.credentials_json')
+            );
 
-        // try {
-        //     if (empty(config('google_calendar.calendar_id'))) {
-        //         Log::warning('Calendar ID no configurado. Omitiendo sincronización.');
-        //         return null;
-        //     }
+            $this->service = new Calendar($this->client);
 
-        //     // Convertir las cadenas de fecha a objetos Carbon primero
-        //     // $start = Carbon::parse($appointment->start);
-        //     // $end = Carbon::parse($appointment->end);
-        //     // Usar las fechas ya convertidas en el modelo
-        //     $start = $appointment->start;
-        //     $end = $appointment->end;
-
-        //     $event = new Event([
-        //         'summary' => $appointment->title,
-        //         'description' => $appointment->description,
-        //         'start' => [
-        //             'dateTime' => $start->format('c'), // Formato ISO 8601
-        //             'timeZone' => config('app.timezone'),
-        //         ],
-        //         'end' => [
-        //             'dateTime' => $end->format('c'), // Formato ISO 8601
-        //             'timeZone' => config('app.timezone'),
-        //         ],
-        //     ]);
-
-        //     if ($appointment->google_calendar_event_id) {
-        //         $event = $this->service->events->update(
-        //             config('google_calendar.calendar_id'),
-        //             $appointment->google_calendar_event_id,
-        //             $event
-        //         );
-        //     } else {
-        //         $event = $this->service->events->insert(
-        //             config('google_calendar.calendar_id'),
-        //             $event
-        //         );
-
-        //         $appointment->update([
-        //             'google_calendar_event_id' => $event->getId()
-        //         ]);
-        //     }
-
-        //     return $event;
-        // } catch (\Exception $e) {
-        //     \Log::error('Error al sincronizar cita con Google Calendar: ' . $e->getMessage());
-        //     throw $e;
-        // }
-    }
-
-
-
-    public function deleteFromCalendar(Appointment $appointment)
-    {
-        if ($appointment->google_calendar_event_id) {
-            try {
-                $this->service->events->delete(
-                    config('google_calendar.calendar_id'),
-                    $appointment->google_calendar_event_id
-                );
-                $appointment->google_calendar_event_id = null;
-                $appointment->save();
-            } catch (\Exception $e) {
-                \Log::error('Google Calendar delete error: ' . $e->getMessage());
-                throw $e;
-            }
+        } catch (\Exception $e) {
+            Log::error('Error inicializando Google Client: ' . $e->getMessage());
+            throw $e;
         }
     }
 
-    public function syncAllAppointments()
+    public function syncAppointment(Appointment $appointment)
     {
-        $appointments = Appointment::whereNotNull('google_calendar_event_id')->get();
-        foreach ($appointments as $appointment) {
-            try {
-                $this->syncAppointment($appointment);
-            } catch (\Exception $e) {
-                \Log::error('Error syncing appointment ' . $appointment->id . ': ' . $e->getMessage());
+        try {
+            if (empty($appointment->google_calendar_event_id)) {
+                return $this->createEvent($appointment);
             }
+
+            return $this->updateEvent($appointment);
+
+        } catch (\Exception $e) {
+            Log::error('Error sincronizando cita: ' . $e->getMessage());
+            throw $e;
+        }
+    }
+
+    protected function createEvent(Appointment $appointment)
+    {
+        $event = new GoogleEvent([
+            'summary' => $appointment->title,
+            'description' => $appointment->description,
+            'start' => $this->getDateTimeObject($appointment->start),
+            'end' => $this->getDateTimeObject($appointment->end),
+        ]);
+
+        $createdEvent = $this->service->events->insert(
+            config('google-calendar.calendar_id'),
+            $event
+        );
+
+        $appointment->update([
+            'google_calendar_event_id' => $createdEvent->getId()
+        ]);
+
+        return $createdEvent;
+    }
+
+    protected function updateEvent(Appointment $appointment)
+    {
+        $event = $this->service->events->get(
+            config('google-calendar.calendar_id'),
+            $appointment->google_calendar_event_id
+        );
+
+        $event->setSummary($appointment->title);
+        $event->setDescription($appointment->description);
+        $event->setStart($this->getDateTimeObject($appointment->start));
+        $event->setEnd($this->getDateTimeObject($appointment->end));
+
+        return $this->service->events->update(
+            config('google-calendar.calendar_id'),
+            $event->getId(),
+            $event
+        );
+    }
+
+    protected function getDateTimeObject($dateTime)
+    {
+        return new \Google\Service\Calendar\EventDateTime([
+            'dateTime' => Carbon::parse($dateTime)->format('c'),
+            'timeZone' => config('app.timezone'),
+        ]);
+    }
+
+    public function deleteFromCalendar(Appointment $appointment)
+    {
+        if (!$appointment->google_calendar_event_id) {
+            return;
+        }
+
+        try {
+            $this->service->events->delete(
+                config('google-calendar.calendar_id'),
+                $appointment->google_calendar_event_id
+            );
+
+            $appointment->update(['google_calendar_event_id' => null]);
+
+        } catch (\Exception $e) {
+            Log::error('Error eliminando evento: ' . $e->getMessage());
+            throw $e;
         }
     }
 
     public function testConnection()
     {
         try {
-            $client = new Client();
-            $client->setAuthConfig(config('google-calendar.auth_profiles.service_account.credentials_json'));
+            // Verificar conexión básica
+            $calendar = $this->service->calendars->get(config('google-calendar.calendar_id'));
 
-            // $client->setAuthConfig(storage_path(config('app/google-calendar/client_secret.json')));
-            $client->addScope(Calendar::CALENDAR);
+            // Crear evento de prueba
+            $event = new GoogleEvent([
+                'summary' => 'Test Connection',
+                'start' => $this->getDateTimeObject(now()),
+                'end' => $this->getDateTimeObject(now()->addHour()),
+            ]);
 
-            if (!$client->getAccessToken()) {
-                // return response()->json([
-                //     'status' => 'auth_required',
-                //     'auth_url' => $client->createAuthUrl()
-                // ]);
-            }
+            $createdEvent = $this->service->events->insert(
+                config('google-calendar.calendar_id'),
+                $event
+            );
 
-            $service = new Calendar($client);
-            $calendar = $service->calendars->get(config('google-calendar.calendar_id'));
+            // Limpiar: eliminar evento de prueba
+            $this->service->events->delete(
+                config('google-calendar.calendar_id'),
+                $createdEvent->getId()
+            );
 
-            // return response()->json([
-            //     'status' => 'success',
-            //     'calendar' => $calendar
-            // ]);
-
-            $event = new Event();
-
-            $event->name = "Test Event";
-            $event->startDateTime = now();
-            $event->endDateTime = now()->addHours(2);
-            $event->summary = 'Test Event';
-            $event->save();
-
-            return Inertia::render('Trainer/TrainerPPlans', ['success' => true]);
-
+            return Inertia::render('Trainer/TrainerPPlans', [
+                'success' => true,
+                'calendar' => [
+                    'id' => $calendar->getId(),
+                    'summary' => $calendar->getSummary()
+                ]
+            ]);
 
         } catch (\Exception $e) {
+            Log::error('Test Connection Failed: ' . $e->getMessage());
+
             return Inertia::render('Trainer/TrainerPPlans', [
                 'success' => false,
-                'error' => $e->getMessage()
+                'error' => $e->getMessage(),
+                'debug' => [
+                    'credentials_path' => config('google-calendar.auth_profiles.service_account.credentials_json'),
+                    'file_exists' => file_exists(config('google-calendar.auth_profiles.service_account.credentials_json')),
+                    'calendar_id' => config('google-calendar.calendar_id')
+                ]
             ]);
         }
     }
