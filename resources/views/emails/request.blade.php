bb7f124a9db3de1c8c1ae8947609e2d46ca03871
Lernik Gasparyan <lernik10@gmail.com>
2025-05-22 18:47:59 +0200

    1

--- resources/views/emails/request.blade.php
+++ resources/views/emails/request.blade.php
@@ -1,15 +1,12 @@
-<x-mail::message>
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <title>Solicitud recibida</title>   
-    </head>
+<head>
+    <title>Solicitud recibida</title>
+</head>
 
-    <body>
-        <h1>Hemos recibido su solicitud</h1>
-        <p>Hola {{ $name }}, hemos recibido su solicitud correctamente.</p>
-        <p>Si tienes alguna duda, responde a este correo o contacta con nuestro equipo de soporte.</p>
-    </body>
-
-</x-mail::message>
+<body>
+    <h1>Hemos recibido su solicitud</h1>
+    <p>Hola {{ $name }}, hemos recibido su solicitud correctamente.</p>
+    <p>Si tienes alguna duda, responde a este correo o contacta con nuestro equipo de soporte.</p>
+</body>
